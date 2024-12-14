7ed6971160a2dacc11f6ddbfd801fa6edd626193
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2024-12-14 15:26:58 +0700

    Add Identity

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SmartCards.DTOs.Account;
+using SmartCards.Interfaces;
+using SmartCards.Models;
+
+namespace SmartCards.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDTO.EmailOrUsername
+                                                                    || x.Email == loginDTO.EmailOrUsername);
+            if (user == null) return Unauthorized("Email hoặc Username không đúng!");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password!, false);
+            if (!result.Succeeded) return Unauthorized("Email/Username hoặc mật khẩu không đúng!");
+
+            return Ok(
+                new NewUserDTO
+                {
+                    Username = user.UserName ?? "",
+                    Email = user.Email ?? "",
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                var appUser = new AppUser
+                {
+                    UserName = registerDTO.Username,
+                    Email = registerDTO.Email
+                };
+                var createUser = await _userManager.CreateAsync(appUser, registerDTO.Password!);
+
+                if (createUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                        return Ok(
+                            new NewUserDTO
+                            {
+                                Username = appUser.UserName!,
+                                Email = appUser.Email!,
+                                Token = _tokenService.CreateToken(appUser)
+                            }
+                        );
+                    else
+                        return StatusCode(500, roleResult.Errors);
+                }
+                else
+                {
+                    return StatusCode(500, createUser.Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"TargetSite: {ex.TargetSite}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
